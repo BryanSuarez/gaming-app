c3af3f10ecdbd81ee700a75ac9d573f417e07f0c
Bryan Suárez <bryanbolivar.1992@gmail.com>
2020-08-19 01:44:02 -0500

    styling index
    create layout design and index view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,60 +10,9 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-    <!-- Styles -->
-    <style>
-        html,
-        body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
+    <link rel="stylesheet" href="/css/main.css">
 
-        .full-height {
-            height: 100vh;
-        }
 
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links>a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-    </style>
 </head>
 
 <body>
@@ -82,7 +31,7 @@
         </div>
         @endif
 
-        <div class="content">
+        <div class="content bg-blue-500">
             <div class="title m-b-md">
                 Laravel Livewire Project
             </div>
