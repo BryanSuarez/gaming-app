45a3ecf7e7ffa950d1c31cafda9d4a57b5700de9
Bryan Suárez <bryanbolivar.1992@gmail.com>
2020-08-22 00:57:17 -0500

    add browsersync
    add plugin to install browsersync

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,8 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.js("resources/js/app.js", "public/js").postCss(
-    "resources/css/main.css",
-    "public/css",
-    [require("tailwindcss")]
-);
+mix.js("resources/js/app.js", "public/js")
+    .postCss("resources/css/main.css", "public/css", [require("tailwindcss")])
+    .browserSync({
+        proxy: "livewire.test"
+    });
